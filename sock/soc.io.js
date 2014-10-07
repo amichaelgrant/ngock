e6779b2172610fac8ffe6108624fe2ffd7d04f5a
amichaelgrant <a.michael.grant@gmail.com>
2014-10-06 18:35:06 -0600

    crisscross detect

--- sock/soc.io.js
+++ sock/soc.io.js
@@ -2,43 +2,50 @@ var util          = require('util');
 var cookie        = require('cookie');
 var cookieParser  = require('cookie-parser');
 var config        = require('../bin/config');
+var uuid          = require('node-uuid');
+var assert        = require('assert');
 
+
+var UUID = "SERVER3000";
 
 var IO = function( io ){
     console.log('io successfully attached to server object');
-    io.use(function(socket, next){
-        //a middle-ware for doing global stuff for all in coming requests on io//
-       //like authentication//
-       console.log('io#use');
-       if( socket.request.headers.cookie){
-            //keep proccessing if there is a cookie//
-            console.log( util.inspect(socket.request.headers.cookie ));
-            var parsedCookies = cookie.parse( socket.request.headers.cookie );
-            var upc_sid = cookieParser.signedCookie( parsedCookies['connect.sid'] , config.SECRET);
-            var upc_io  = cookieParser.signedCookie( parsedCookies['io'] , config.SECRET);
-            console.log('----------------------------------');
-            console.log( util.inspect( parsedCookies ));
-            console.log('----------------------------------');
-            console.log( 'upc_sid - ' + upc_sid );
-            console.log('----------------------------------');
-            console.log( 'upc_io - ' + upc_io );
+//    io.use(function(socket, next){
+//        //a middle-ware for doing global stuff for all in coming requests on io//
+//       //like authentication//
+//       console.log('io#use');
+//       if( socket.request.headers.cookie){
+//            //keep proccessing if there is a cookie//
+//            console.log( util.inspect(socket.request.headers.cookie ));
+//            var parsedCookies = cookie.parse( socket.request.headers.cookie );
+//            var upc_sid = cookieParser.signedCookie( parsedCookies['connect.sid'] , config.SECRET);
+//            var upc_io  = cookieParser.signedCookie( parsedCookies['io'] , config.SECRET);
+//            console.log('----------------------------------');
+//            console.log( util.inspect( parsedCookies ));
+//            console.log('----------------------------------');
+//            console.log( 'upc_sid - ' + upc_sid );
+//            console.log('----------------------------------');
+//            console.log( 'upc_io - ' + upc_io );
+//
+//            //make parsed session id 'connect.sid' available//
+//            socket.SESSIONID = upc_sid;
+//            return next();
+//       }else
+//            next( new Error('Authentication and Authorization required'));
+//    });
 
-            //make parsed session id 'connect.sid' available//
-            socket.SESSIONID = upc_sid;
-            return next();
-       }else
-            next( new Error('Authentication and Authorization required'));
-    });
     io.on('connection', function (socket) {
       //io.emit('server', 'To all clients connected to io');
-      socket.emit('server', 'To the specific client who initiated the connection');
+      socket.emit('server', UUID);
 
       socket.on('client', function (data) {
         //recieve and process all communication related to the specific client//
         //who initiated the connection;//
-        console.log('socket.SESSIONID : ' + socket.SESSIONID);
-        console.log('Data from client : ' + data);
-        socket.emit('server', data);
+        console.log('CLIENT : ' + data);
+
+        if( data && data.indexOf(UUID) == -1)
+            console.log('CRISSCROSS occured');
+        socket.emit('server', UUID );
       });
     
       socket.on('disconnect', function () {
